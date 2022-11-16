5d9b682dc2ff151a104313e90ac6bde5adbaeff8
Muhammet Burak Golec <mozartburak42@gmail.com>
2022-11-16 13:57:19 +0300

    Test
    v0.0.1a

--- Nmap.py
+++ Nmap.py
@@ -3,12 +3,18 @@
 # Compare this snippet from main.py:
 # #!/bin/env/python3.10
 
-import nmap3 as nmap
+import nmap
 
-def __init__(self, *args, **kwargs):
-    pass
 
-def scan(self, target):
-    nmap = nmap.Nmap()
-    results = nmap.scan_top_ports(target)
-    return results
+def __init__(self, *target, port, t_out):
+    self.target = target.get('target', None)
+    self.port = port.get('port', None)
+    self.t_out = t_out.get('t_out', None)
+    return self.scan(self.target, self.port, self.t_out)
+
+def scan(self, target, port = '1-65535', t_out = 10):
+    self.target = target
+    nm = nmap.PortScanner()
+    # scan all ports
+    nm.scan(target, port, timeout=t_out)
+    return nm
