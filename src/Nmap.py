b3c48192ad9cbf039c863ff0ee3a3be8437ef956
Muhammet Burak Golec <mozartburak42@gmail.com>
2022-11-23 12:49:51 +0300

    Nmap-Class

--- src/Nmap.py
+++ src/Nmap.py
@@ -2,17 +2,18 @@
 # Compare this snippet from main.py:
 # #!/bin/env/python3.10
 
+
 import sys
 import subprocess
 
 
 class scannmap:
-    def __init__(self, target, port, request='sudo', options='-sU -sT -sV -O -p'):
+    def __init__(self, target: str, port: str, request: str, options :str):
         self.target = target
         self.port = port
         self.request = request
         self.options = options
-
+        dict = { "target": self.target, "port": self.port, "request": self.request, "options": self.options}
 
     def scan(self):
         if (self.target == None):
@@ -30,10 +31,18 @@ class scannmap:
             print("Please enter a port")
             sys.exit(1)
 
-        elif (self.options != None)
+        elif (self.options != None):
             print("Options Set")
             print(self.request,'nmap' ,self.target,self.options, self.port)
-            return subprocess.call([self.request,'nmap' ,self.target,self.options, self.port])
+
+            try:
+                Scan_Value = subprocess.call([self.request,'nmap' ,self.target,self.options, self.port])
+                Scan_Value = str(Scan_Value)
+                print(Scan_Value)
+                return Scan_Value
+            except subprocess.CalledProcessError as e:
+                print("Error: ", e.output)
+                sys.exit(1)
         
         else:
             print("OPTIONS NOT SET")
