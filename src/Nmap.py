0185afce841ac1781fceecd8cf7652116a9c4821
Muhammet Burak Golec <mozartburak42@gmail.com>
2022-11-23 15:06:45 +0300

    Some Logic Changes

--- src/Nmap.py
+++ src/Nmap.py
@@ -13,9 +13,9 @@ class scannmap:
         self.port = port
         self.request = request
         self.options = options
-        dict = { "target": self.target, "port": self.port, "request": self.request, "options": self.options}
-
-    def scan(self):
+        self.dict = { "target": self.target, "port": self.port, "request": self.request, "options": self.options}
+        self.scan_approuved = False
+        
         if (self.target == None):
             print("Please enter a target")
             sys.exit(1)
@@ -30,19 +30,39 @@ class scannmap:
         elif (self.port == None):
             print("Please enter a port")
             sys.exit(1)
-
+        
         elif (self.options != None):
             print("Options Set")
+        
+        else:
+            self.scan_approuved = True
+            print("Scan Approuved")
+
+    def scan(self):
+        if (self.scan_approuved == True):
+
             print(self.request,'nmap' ,self.target,self.options, self.port)
 
             try:
                 Scan_Value = subprocess.call([self.request,'nmap' ,self.target,self.options, self.port])
                 Scan_Value = str(Scan_Value)
                 print(Scan_Value)
-                return Scan_Value
+                return Scan_Value, self.dict
+
             except subprocess.CalledProcessError as e:
                 print("Error: ", e.output)
                 sys.exit(1)
-        
+
+            else:
+                print("Unexpected error:", sys.exc_info()[0])
+                sys.exit(0)            
+            
+            finally:
+                print("Scan PID Completed")
+
         else:
             print("OPTIONS NOT SET")
+
+    def scan_script(self):
+        pass
+        
