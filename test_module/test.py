8d4b9dc2f5bacc96959145bfa5e67e8db9f6ebd5
Muhammet Burak Golec <mozartburak42@gmail.com>
2022-12-06 10:57:57 +0300

    add test_module

---
+++ test_module/test.py
@@ -0,0 +1,18 @@
+import Output, Split, Convert, Vuln_port
+
+def main():
+    print ("Test Starting...")
+    Output.main()
+    print ("Test Split...")
+    Split.main()
+    print ("Test Convert...")
+    Convert.main()
+    print ("Test Vuln_port...")
+    Vuln_port.main()
+    print ("Test Finished...")
+
+
+if __name__ == '__main__':
+    main()
+
+
